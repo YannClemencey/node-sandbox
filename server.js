7e4c9296c1fcf6d33d780312e53252d68bcbdac9
YannClemencey <y.clemencey@gmail.com>
2022-11-21 20:03:21 +0100

    MEP du serveur

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+/**
+ * Récupérations des packages
+ */
 const express = require('express')
+const path = require('path')
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000)
